09cc81fc8b4c61ee95b10d4f9b43ef2bbae29851
Jonas Lauer <jonas.lauer@bizerba.com>
2022-09-29 09:00:00 +0200

    Use Google Tag Manager

---
+++ _includes/scripts/gtm.js
@@ -0,0 +1,19 @@
+(function() {
+  var gtm = document.createElement('noscript');
+  var frm = document.createElement('iframe');
+  frm.width=0;
+  frm.height=0;
+  frm.style="display:none;visibility:hidden";
+  frm.src="https://www.googletagmanager.com/ns.html?id=GTM-NJJTMZB";
+  gtm.appendChild(frm);
+  var s = document.getElementsByTagName('body')[0];
+  s.insertBefore(gtm, s.children[0]);
+})();
+
+//Google Tag Manager
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NJJTMZB');
+//End Google Tag Manager
